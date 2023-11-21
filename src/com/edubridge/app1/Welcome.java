0bf285231fb3e608cef75b3bb789bef0fb8062ae
ShravyaMandaloju <151602262+ShravyaMandaloju@users.noreply.github.com>
2023-11-21 11:26:22 +0530

    Update Welcome.java

--- src/com/edubridge/app1/Welcome.java
+++ src/com/edubridge/app1/Welcome.java
@@ -2,7 +2,7 @@ package com.edubridge.app1;
 
 public class Welcome {
 	public static void main(String[] args) {
-		System.out.println("Welcome to java");
+		System.out.println("Welcome to the java");
 	}
 
 }
